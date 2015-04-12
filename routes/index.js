3eae9db0e0bbcd301cc71a51726d6839ab5f3d1b
Troy Connor <troy.connor@yahoo.com>
2015-04-11 23:23:03 -0400

    Got data to the page.

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var _ = require('lodash');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  var url = "https://data.oregon.gov/api/views/7zxm-9fbf/rows.json";
+  request.get(url, function(err,response,body) {
+    if(err) {
+      console.log(err);
+    }
+    var cols = JSON.parse(body).meta.view.columns;
+    var columns = _.pluck(cols, 'name');
+    var data = JSON.parse(body).data;
+    res.render('index', { columns: columns, title:'Open Data', data:data });
+  })
+
+});
+
+module.exports = router;
